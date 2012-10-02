0f1c3370ac0591fbd5c4f7faa899b2f8d3095de7
Stefan Becker <stefan.becker@mv.uni-kl.de>
2012-10-02 11:40:03 +0000

    new shape of liquid cuboid
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/branches/density-profiles@1462 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- tools/mksd/GlobalStartGeometry.cpp
+++ tools/mksd/GlobalStartGeometry.cpp
@@ -58,8 +58,8 @@ void GlobalStartGeometry::calculateBoxFluidOffset(double hWall, double shielding
 	_box[0] = nEkX * LATTICE_CONST_WALL_LJTS;
 	_box[2] = nEkZ * LATTICE_CONST_WALL_LJTS;
 
-	_effLiq[0] = _box[0] / _alpha;
-	_effLiq[2] = _box[2] / _gamma;
+	_effLiq[0] =pow(2.0 , 1.0/3.0) * _box[0] / _alpha;
+	_effLiq[2] =pow(2.0 , 1.0/3.0) * _box[2] / _gamma;
 	_effLiq[1] = (_nLiq / _rhoLiq) / (_effLiq[0]*_effLiq[2]);
 /*	cout << "effLiq[0]: " << effLiq[0] << "\n";
 	cout << "effLiq[1]: " << effLiq[1] << "\n";
