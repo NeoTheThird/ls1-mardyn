e67039e5ba72a3060077730b0fb818418c338433
Wolfgang Eckhardt <eckhardw@in.tum.de>
2014-01-03 22:18:31 +0000

    fix initialisation of checkpoint to avoid mean deadlock
    git-svn-id: https://scm.projects.hlrs.de/svn/ls1/MarDyn/branches/WolfgangEckhardt-measure@2325 a63bd714-7e14-4b5e-94e7-302c8c8ff188

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -84,7 +84,7 @@ using namespace std;
 Simulation* global_simulation;
 
 Simulation::Simulation() :
-	_rdf(NULL), _ljFlopCounter(NULL), _domainDecomposition(NULL) {
+	_rdf(NULL), _ljFlopCounter(NULL), _domainDecomposition(NULL), _forced_checkpoint_time(0) {
 
 	initialize();
 }
@@ -954,7 +954,7 @@ void Simulation::initConfigOldstyle(const string& inputfilename) {
 	global_log->info() << "xx quadrupole present: " << quadrupole_present << endl;
 	global_log->info() << "xx tersoff present: " << tersoff_present << endl;
 
-#if 0
+#if 1
 	_cellProcessor = new LegacyCellProcessor( _cutoffRadius, _LJCutoffRadius, _tersoffCutoffRadius, _particlePairsHandler);
 #else
 	if (dipole_present || quadrupole_present || tersoff_present) {
